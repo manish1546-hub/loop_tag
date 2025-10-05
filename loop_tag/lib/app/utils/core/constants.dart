0b5952795de6140481931c619d00f3048adc4628
leon-ronaldo <leonronaldo.trs@gmail.com>
2025-10-05 22:22:15 +0530

    update: ic_launcher app icon

--- loop_tag/lib/app/utils/core/constants.dart
+++ loop_tag/lib/app/utils/core/constants.dart
@@ -1,6 +1,7 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
 
-const String baseURI = "http://10.0.2.2:8055/api";
+// const String baseURI = "http://10.0.2.2:8055/api";
+const String baseURI = "https://loop-tag.onrender.com/api";
 
 Future<bool> isConnected() async {
   return (await Connectivity().checkConnectivity()).first !=
